f87f8ffa7acb71993926610d7a89ef9a56a24608
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2019-06-08 21:17:55 +0100

    Fixed bib file processing
    + checking for duplicates

--- bib_processing.py
+++ bib_processing.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+from difflib import SequenceMatcher
 
 
 supported_bibtex_types = {"article", "book", "booklet", "inbook", "incollection", "inproceedings", "manual",
@@ -15,6 +17,11 @@ supported_fields = ["author", "title", "year", "month", "pages", "note",
 extra_fields = ["doi", "issn", "isbn", "keywords", "abstract", "url", "archivePrefix", "eprint", "timestamp", "biburl",
                 "bibsource"]
 
+data_path = Path("_data/papers.yml")
+bib_path = Path("bibfiles")
+year_from = 2017
+similarity_threshold = 0.8
+
 
 def find_all_files(path_to_search):
     """Recursively find all bib files in root path given"""
@@ -23,11 +30,11 @@ def find_all_files(path_to_search):
     # Iterate over all the entries
     for e in list_of_files:
         # Create full path
-        full_path = os.path.join(path_to_search, e)
+        full_path = path_to_search / e
         # If entry is a directory then get the list of files in this directory
         if os.path.isdir(full_path):
             all_files = all_files + find_all_files(full_path)
-        elif full_path.endswith(".bib"):
+        elif full_path.with_suffix(".bib"):
             all_files.append(full_path)
 
     return all_files
@@ -78,59 +85,125 @@ def process_entry(entry_to_process):
 
 
 def find_pdf(entry_id, year):
+    """
+    Returns true if a pdf for this paper exists in the pdf/pub/year directory (must have name as paper ID)
+    """
     return os.path.isfile("pdf/pub/" + year + "/" + entry_id + ".pdf")
 
 
-def format_entry(entry):
+def output_entries(entries):
     """
-    Prints the given bibtex entry into yaml supported format
-    :param entry
+    Prints the given bibtex entries into yaml supported format
     """
-    with open("output.yml", 'a+') as wf:
-        wf.write("-   id: " + entry["id"] + "\n")
-        for e in entry:
-            if e != "id":
-                if ":" in entry[e]:
-                    entry[e] = '"' + entry[e] + '"'
-                wf.write("    " + e + ": " + entry[e] + "\n")
+    with open(data_path.absolute(), 'w+', encoding='utf-8') as wf:
+        for entry in entries:
+            if int(entry["year"]) < year_from:
+                continue
+            wf.write("-   id: " + entry["id"] + "\n")
+            for e in entry:
+                if e != "id":
+                    if ":" in entry[e]:
+                        entry[e] = '"' + entry[e] + '"'
+                    wf.write("    " + e + ": " + entry[e] + "\n")
+
+
+def check_equality(entry1, entry2):
+    """
+    Checks if 2 entries are the same
+    """
+    sim_fields = 0
+    common_fields = 0
+    for field1 in entry1:
+        for field2 in entry2:
+            if field1 == field2:
+                common_fields += 1
+                if similar(entry1[field1], entry2[field2]) >= similarity_threshold:
+                    sim_fields += 1
+    if common_fields == 0:
+        return False
+    if sim_fields / common_fields >= similarity_threshold:
+        return True
+    return False
+
+
+def similar(a, b):
+    """
+    Checks if 2 strings are similar, returns a similarity measure.
+    """
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def process_yml_entries(lines):
+    """
+    Processes entries in yml format
+    :param lines: list of lines from yml file to process
+    :return: list of entries as dictionaries
+    """
+    entry_list = []
+    entry = {}
+    ln = 0
+    for line in lines:
+        if "-   id:" in line or ln == len(lines) - 1:  # Starting a new entry
+            if len(entry) > 0:
+                entry_list.append(entry)
+            entry = {}
+        line = line.replace("\"", "")
+        if "-   id:" in line:
+            line = line[1:]  # Ignore first dash
+        stripped_line = line.strip()
+        if stripped_line != "":  # Adding to current entry
+            split_line = stripped_line.split(':')
+            entry[split_line[0].strip()] = ':'.join(split_line[1:]).strip()
+        ln += 1
+    return entry_list
 
 
 def main():
     """
     Main function to process bibtex entries in a given path and output a file in yaml supported format.
     """
-    path = "bibfiles"
-    files = find_all_files(path)
-    abs_path = os.path.abspath(os.path.dirname(__file__))
+    # Read in current entries
+    lines = data_path.read_text(encoding='utf-8').split('\n')
+    entries = process_yml_entries(lines)
 
-    entries = []
+    # Find new entries
+    files = find_all_files(bib_path)
+
     for bibfile in files:
         entry = ""
-        full_pth = os.path.join(abs_path, bibfile).replace("\\", "\\\\")
-        try:
-            with open(full_pth) as f:
-                lines = f.readlines()
-                line_number = 0
-                for line in lines:
-                    if "@" in line or line_number == len(lines)-1:  # Starting a new entry
-                        if entry != "":
-                            entries.append(entry)
-                        entry = ""
-                    if "@" in line:
-                        line = line.replace("{", "=")
-                    stripped_line = line.strip()
-                    if stripped_line != "":  # Adding to current entry
-                        if stripped_line.endswith(","):
-                            stripped_line = stripped_line[:-1]
-                        entry += stripped_line + "\n"
-                    line_number += 1
-        except:
-            print("File " + full_pth + " not found.")
+        full_pth = Path(bibfile)
+        lines = full_pth.read_text(encoding='utf-8').split('\n')
+        line_number = 0
+        for line in lines:
+            if "@" in line or line_number == len(lines)-1:  # Starting a new entry
+                if entry != "":
+                    entry = entry.translate({ord(c): None for c in '\\"{}~\'"'})
+                    processed_entry = process_entry(entry)
+                    entries.append(processed_entry)
+                entry = ""
+            if "@" in line:
+                line = line.replace("{", "=")
+            stripped_line = line.strip()
+            if stripped_line != "":  # Adding to current entry
+                if stripped_line.endswith(","):
+                    stripped_line = stripped_line[:-1]
+                entry += stripped_line + "\n"
+            line_number += 1
 
-    for entry in entries:
-        entry = entry.translate({ord(c): None for c in '\\"{}~\''})
-        processed_entry = process_entry(entry)
-        format_entry(processed_entry)
+    # Check for duplication
+    duplicate_entries = []
+    for i in range(len(entries)-1):
+        for j in range(i+1, len(entries)):
+            if check_equality(entries[i], entries[j]):
+                print("Duplicate found: " + entries[i]["id"] + " = " + entries[j]["id"])
+                duplicate_entries.append(j)
+    duplicate_entries.sort()
+    for i in range(len(duplicate_entries)):
+        e = duplicate_entries[i] - i
+        del entries[e]
+
+    # Finally, save entries
+    output_entries(entries)
 
 
 if __name__ == "__main__":
